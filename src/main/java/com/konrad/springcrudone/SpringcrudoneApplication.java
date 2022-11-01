0934ca0025ddd5eba5fa4c8f91ffca2de2ed9279
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2022-11-01 18:28:02 +0100

    first commit

---
+++ src/main/java/com/konrad/springcrudone/SpringcrudoneApplication.java
@@ -0,0 +1,13 @@
+package com.konrad.springcrudone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrudoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konrad/springcrudone/SpringcrudoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.springcrudone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
